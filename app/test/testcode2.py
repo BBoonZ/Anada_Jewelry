e1574ecab6642720eea31d41097fc0823e39d13a
BBoonZ <bosszaza0380@gmail.com>
2024-10-05 21:57:06 +0700

    Update kai

--- app/test/testcode2.py
+++ app/test/testcode2.py
@@ -15,13 +15,29 @@ cursor = conn.cursor()
 # เส้นทางของไฟล์ภาพที่คุณต้องการเพิ่ม
 # Construct the absolute path to the img folder
 img_folder_path = Path(__file__).resolve().parent.parent / 'img'
-image_path = img_folder_path / 'hippo.jpg'  # แทนที่ด้วยเส้นทางไฟล์ภาพของคุณ
+image_name = "kai"
+
+image_path_jpg = img_folder_path / f'{image_name}.jpg'
+image_path_jpeg = img_folder_path / f'{image_name}.jpeg'
+image_path_png = img_folder_path / f'{image_name}.png'
+
+if image_path_jpg.exists():
+    image_path = image_path_jpg
+    image_name = f'{image_name}.jpg'
+elif image_path_jpeg.exists():
+    image_path = image_path_jpeg
+    image_name = f'{image_name}.jpeg'
+elif image_path_png.exists():
+    image_path = image_path_png
+    image_name = f'{image_name}.png'
+else:
+    raise FileNotFoundError("Image not found in .jpg, .jpeg, or .png formats")
 
 # แปลงภาพเป็นไบนารี
 image_data = image_to_binary(image_path)
 
 # รันคำสั่ง INSERT เพื่อเพิ่มข้อมูลภาพ
-cursor.execute('INSERT INTO product (name, file_pic, pic, stock_quantity) VALUES ("hippo", ?, ?, 5)', ('hippo.jpg', image_data))
+cursor.execute('INSERT INTO product (name, file_pic, pic, stock_quantity) VALUES ("kai", ?, ?, 5)', (image_name, image_data))
 
 # บันทึกการเปลี่ยนแปลง
 conn.commit()
