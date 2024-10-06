59847326b857804769830d1e30927fe04782459e
kittipith <66070018@kmitl.ac.th>
2024-10-06 17:35:00 +0700

    หน้า login
    เดี๋ยวเก็บรายละเอียดอีกที

---
+++ website/static/javascript/stock.js
@@ -0,0 +1,7 @@
+document.querySelector('.button_sale_record').addEventListener('click', function() {
+    window.location.href = 'sale_record.html';
+});
+
+document.querySelector('.dropdown_Stock_class').addEventListener('click', function() {
+    window.location.href = 'stock.html';
+});
