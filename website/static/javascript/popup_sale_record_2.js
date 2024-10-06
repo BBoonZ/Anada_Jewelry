2e24e9e044db21a0d588fc0761203707f2d0fb99
kittipith <66070018@kmitl.ac.th>
2024-10-06 20:59:36 +0700

    แก้หน้าตะกร้า

--- website/static/javascript/popup_sale_record_2.js
+++ website/static/javascript/popup_sale_record_2.js
@@ -6,3 +6,20 @@ function goBackToMainPage() {
     closePopup2();
     window.parent.location.reload();
 }
+
+function checkQuantity(input) {
+    if (input.value.length > 3) {
+        input.value = input.value.slice(0, 3);
+    }else if (input.value < 1) {
+        input.value = 1;
+    }
+}
+
+function preventRemoveSymbol(input) {
+    const originalValue = "฿ ";
+    if (!input.value.startsWith("฿")) {
+        input.value = originalValue;
+    } else if (input.value.length < originalValue.length) {
+        input.value = originalValue;
+    }
+}
